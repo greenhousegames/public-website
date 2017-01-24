d3410bce65c3bd9a4c50871cfbbb72da95359ca9
Brian Soumakian <bsoumakian@gmail.com>
2017-01-23 18:08:54 -0800

    update acceleration page

--- app/pages/learn/abc/a.js
+++ app/pages/learn/abc/a.js
@@ -16,9 +16,13 @@ var game = new Phaser.Game(width, width/(16/9), Phaser.AUTO, 'learning-game', {
     sprite.body.allowRotation = false;
   },
   update: () => {
-    sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
+    game.physics.arcade.accelerateToPointer(sprite, game.input.activePointer, 100, 100, 0);
   },
   render: () => {
-    game.debug.spriteInfo(sprite, 32, 32);
+    var pos = 24;
+    game.debug.text("velocity: " + sprite.body.velocity.x, 16, pos, "#ffffff");
+    pos += 16;
+    game.debug.text("acceleration: " + sprite.body.acceleration.x, 16, pos, "#ffffff");
+    pos += 16;
   }
 });
