2cb8f2c62613f1eb7b499b6b4bded70fa5013e89
Brian Soumakian <bsoumakian@gmail.com>
2017-02-18 01:01:09 -0800

    add Jump and Weapon lessons

--- app/pages/learn/abc/j.js
+++ app/pages/learn/abc/j.js
@@ -9,9 +9,15 @@ function create() {
     },
     create: () => {
       utils.create(game);
+      game.physics.arcade.gravity.y = 200;
 
       sprite1 = game.add.sprite(game.width/2, game.height/2, 'greenhouse');
+      game.physics.enable(sprite1, Phaser.Physics.ARCADE);
       sprite1.anchor.setTo(0.5, 0.5);
+      sprite1.body.collideWorldBounds = true;
+      sprite1.body.bounce.set(0);
+
+      game.input.onDown.add(jump);
     },
     update: () => {
     },
@@ -19,6 +25,22 @@ function create() {
     }
   });
   return game;
+
+  function jump() {
+    if (sprite1.y == (game.height - utils.getIconWidth(game)/2)) {
+      switch (utils.getBreakpoint(game)) {
+        case 'large':
+          sprite1.body.velocity.y = -300;
+          break;
+        case 'medium':
+          sprite1.body.velocity.y = -250;
+          break;
+        case 'small':
+          sprite1.body.velocity.y = -200;
+          break;
+      }
+    }
+  }
 }
 
 module.exports = create;
--- app/pages/learn/abc/w.js
+++ app/pages/learn/abc/w.js
@@ -1,17 +1,28 @@
 import utils from './utils.js';
 
 function create() {
-  var sprite1;
+  var sprite1, weapon;
 
   var game = utils.init('w', {
     preload: () => {
       utils.preload(game);
+      game.load.image('bullet', '/assets/img/learning/weapon-bullet.png');
     },
     create: () => {
       utils.create(game);
 
+      weapon = game.add.weapon(30, 'bullet');
+      weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+      weapon.bulletSpeed = 600;
+      weapon.fireRate = 100;
+
       sprite1 = game.add.sprite(game.width/2, game.height/2, 'greenhouse');
       sprite1.anchor.setTo(0.5, 0.5);
+      game.physics.arcade.enable(sprite1);
+
+      weapon.trackSprite(sprite1, 0, 0, false);
+
+      game.input.onDown.add(fire);
     },
     update: () => {
     },
@@ -19,6 +30,11 @@ function create() {
     }
   });
   return game;
+
+  function fire() {
+    weapon.fireAngle = Phaser.Math.radToDeg(game.physics.arcade.angleToPointer(sprite1));
+    weapon.fire();
+  }
 }
 
 module.exports = create;
