b26f556d9e4a3ce4628951a09f1fc51315a1fd1c
Brian Soumakian <bsoumakian@gmail.com>
2017-02-20 06:52:26 -0800

    1.0.18

--- dist/debug/assets/js/app.js
+++ dist/debug/assets/js/app.js
@@ -199,7 +199,19 @@ window.GreenhouseGames = {
       window.GreenhouseGames.authHelpers.hideAuth();
     }
   });
+
+  (0, _jquery2.default)(document).foundation();
 });
+
+// facebook
+(function (d, s, id) {
+  var js,
+      fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1193182344132806";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
 
 });
 
