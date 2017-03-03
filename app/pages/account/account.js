073f6acbe2cc94d1ad1c7e680ef0b8b4a7c27070
Brian Soumakian <bsoumakian@gmail.com>
2017-03-03 14:11:45 -0800

    update canonical links

--- app/pages/account/account.js
+++
@@ -1,4 +0,0 @@
-$('#logout_button').click(() => window.GreenhouseGames.client.signOut().then(window.GreenhouseGames.authHelpers.hideAuth));
-$('#twitterLogin_button').click(() => window.GreenhouseGames.client.signInWithPopup('twitter').then(window.GreenhouseGames.authHelpers.loginSuccess).catch(window.GreenhouseGames.authHelpers.loginError));
-$('#facebookLogin_button').click(() => window.GreenhouseGames.client.signInWithPopup('facebook').then(window.GreenhouseGames.authHelpers.loginSuccess).catch(window.GreenhouseGames.authHelpers.loginError));
-$('#guestLogin_button').click(() => window.GreenhouseGames.client.signInAnonymously().then(window.GreenhouseGames.authHelpers.loginSuccess).catch(window.GreenhouseGames.authHelpers.loginError));
